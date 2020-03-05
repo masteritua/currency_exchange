10c92afc915ff595529b3732f3820ed2ad5ed19a
Aleksey Shevchenko <masteritua@gmail.com>
2020-03-05 13:59:56 +0200

    fix-lesson23

--- feedback/models.py
+++ feedback/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
-class FeedbackModel(models.Model):
-  pass
+class ContactModel(models.Model):
+    email = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
--- feedback/views.py
+++ feedback/views.py
@@ -1,25 +1,29 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import FeedbackModel
+from .models import ContactModel
 from django.views.generic import CreateView
+from currency.tasks import feedback_task
+from django.urls import reverse
 
 
 class FeedbackCreateView(CreateView):
-  model = FeedbackModel
-  fields = ('title', 'description')
+    model = ContactModel
+    fields = ('email', 'title', 'text')
 
-  success_url = "/"
+    success_url = "/"
 
 
 def feedback_views(request):
-  if request.method == 'POST':
+    if request.method == 'POST':
 
-    form = FeedbackModel(request.POST)
-    if form.is_valid():
-      feedback_task.delay(form)
+        form = ContactModel(request.POST)
+        if form.is_valid():
+            form.save();
+            feedback_task.delay(request.POST)
 
-      return HttpResponseRedirect(reverse('feedback'))
+            return HttpResponseRedirect(reverse('feedback'))
 
-  else:
-    form = FeedbackModel(initial=post)
+    else:
+        form = ContactModel(initial=request.POST)
 
-  return render(request, 'feedback.html', context={"form": form})
+    return render(request, 'feedback.html', context={"form": form})
